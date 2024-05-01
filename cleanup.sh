0dfa51eb481b93c1800cf96eb8f99b5a21b56c9d
syxc <nian1@duck.com>
2024-05-02 07:42:45 +0800

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -2,7 +2,9 @@
 rm -rf .idea
 ./gradlew clean
 rm -rf .gradle
+rm -rf build
+rm -rf */build
 rm -rf iosApp/iosApp.xcworkspace
 rm -rf iosApp/Pods
 rm -rf iosApp/iosApp.xcodeproj/project.xcworkspace
-rm -rf iosApp/iosApp.xcodeproj/xcuserdata
+rm -rf iosApp/iosApp.xcodeproj/xcuserdata 
