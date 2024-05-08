ee833ac98b8b791cf937983a13b0d26324d19393
nian1 <nian1@duck.com>
2024-05-09 03:18:07 +0800

    feat(shared): Add iOS-specific counter view implementation

---
+++ shared/src/iosMain/kotlin/com/github/app/shared/IosButton.kt
@@ -0,0 +1,51 @@
+package com.github.app.shared
+
+import app.cash.redwood.Modifier
+import com.ding1ding.app.shared.redwood.widget.Button
+import kotlinx.cinterop.BetaInteropApi
+import kotlinx.cinterop.ExperimentalForeignApi
+import kotlinx.cinterop.ObjCAction
+import platform.UIKit.UIButton
+import platform.UIKit.UIColor
+import platform.UIKit.UIControlEventTouchUpInside
+import platform.UIKit.UIControlStateNormal
+import platform.UIKit.UIView
+import platform.objc.sel_registerName
+
+// NOTE: This class must be public for the click selector to work.
+class IosButton : Button<UIView> {
+  override val value = UIButton().apply {
+    backgroundColor = UIColor.grayColor
+  }
+
+  override var modifier: Modifier = Modifier
+
+  override fun text(text: String?) {
+    value.setTitle(text, UIControlStateNormal)
+  }
+
+  override fun enabled(enabled: Boolean) {
+    value.enabled = enabled
+  }
+
+  @OptIn(ExperimentalForeignApi::class)
+  private val clickedPointer = sel_registerName("clicked")
+
+  @OptIn(BetaInteropApi::class)
+  @ObjCAction
+  fun clicked() {
+    onClick?.invoke()
+  }
+
+  private var onClick: (() -> Unit)? = null
+
+  @OptIn(ExperimentalForeignApi::class)
+  override fun onClick(onClick: (() -> Unit)?) {
+    this.onClick = onClick
+    if (onClick != null) {
+      value.addTarget(this, clickedPointer, UIControlEventTouchUpInside)
+    } else {
+      value.removeTarget(this, clickedPointer, UIControlEventTouchUpInside)
+    }
+  }
+}
